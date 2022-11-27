3da4f3c1c40056fadbae627d33d354673865c1de
gffice <114347535+gffice@users.noreply.github.com>
2022-11-27 12:44:34 +0800

    Update keepnew1.sh

--- keepnew1.sh
+++ keepnew1.sh
@@ -5,34 +5,11 @@ BOT_USER="github-actions[bot]"
 BOT_EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
 CONFIG_PATH="config"
 
+git config user.name "$BOT_USER"
+git config user.email "$BOT_EMAIL"
+git clone https://github.com/fuserh/torproject
+git add .
+git remote add origin https://github.com/gffice/torpage.git
+git branch -M master
+git push -u origin master
 
-sync() {
-    action="$1"
-    message="$2"
-
-    # call windows git from wsl
-    git=git
-    command -v git.exe && git=git.exe
-
-    $git config user.name "$BOT_USER"
-    $git config user.email "$BOT_EMAIL"
-    $git clone https://github.com/fuserh/torproject
-    $git remote add origin https://github.com/gffice/torpage.git
-    $git branch -M master
-    $git push -u origin master
-}
-
-case $1 in
-register)
-    register
-    ;;
-invoke)
-    invoke
-    ;;
-pull | push)
-    sync "$@"
-    ;;
-*)
-    exit 0
-    ;;
-esac
